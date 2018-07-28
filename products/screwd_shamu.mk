eda3717d30b1c4d4f91f3650565d6540cef4915f
wrongway213 <tropeazy@gmail.com>
2018-07-28 21:41:46 +0530

    shamu: Fix vendor mismatch
    This doesn't need to be defined here, it's already defined in device tree.

--- products/screwd_shamu.mk
+++ products/screwd_shamu.mk
@@ -28,12 +28,6 @@ PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
 
-# Device Fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/NGI55D/4298276:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 8.0.0 OPR6.170623.017 4298423 release-keys"
-
 endif
 
 # Kernel inline build
